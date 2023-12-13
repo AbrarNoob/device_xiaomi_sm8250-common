2d0397d933dc3711a59371c1f6b24ded90296738
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-12-13 20:52:11 +0300

    sm8250-common: Link dolby blobs against v33 libstagefright_foundation
     * fixes crashes and absurdly high CPU usage while using dolby
    
    Co-authored-by: Woomymy <woomy@woomy.be>

--- kona.mk
+++ kona.mk
@@ -253,6 +253,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor
 
+# Dolby VNDK libs
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v33/arm/arch-arm-armv7-a-neon/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefright_foundation-v33.so \
+    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33.so
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
